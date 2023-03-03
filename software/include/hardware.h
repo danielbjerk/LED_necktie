13a69d12bf026b436ff33efbcaf6ed6c6dd71694
Daniel Bjerkehagen <daniel.bjerkehagen@gmail.com>
2023-03-03 19:09:18 +0100

    Add code skeleton, move files to src and include

---
+++ software/include/hardware.h
@@ -0,0 +1,16 @@
+/*
+ * hardware_.h
+ *
+ * Created: 03.03.2023 17:11:25
+ *  Author: danie
+ */ 
+
+
+#ifndef HARDWARE'_H_
+#define HARDWARE'_H_
+
+
+
+
+
+#endif /* HARDWARE'_H_ */
---
+++ software/src/hardware.c
@@ -0,0 +1,6 @@
+/*
+ * hardware.c
+ *
+ * Created: 03.03.2023 17:11:42
+ *  Author: danie
+ */ 
